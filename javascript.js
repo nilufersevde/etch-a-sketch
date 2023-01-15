a4ca26500ccc97f38510d57cc0665b8f4e353c05
Nilufer <n.sevde@hotmail.com>
2023-01-15 19:59:15 +0300

    Create a webpage with grid of square divs.

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const grid = document.createElement("div")
+grid.classList.add('grid');
+const container = document.createElement("div")
+container.classList.add('container');
+
+for(var i=1; i < 626; i++){
+   var newDiv = document.createElement('div');
+   newDiv.className = 'cell';
+   grid.appendChild(newDiv);
+}
+
+container.appendChild(grid);
+document.body.appendChild(container);
